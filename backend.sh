a3b1517bb7aeea3af3c69cf1c84585548f2f3b43
kedarnath9988 <anubolukedarnath9988@gmail.com>
2024-10-07 18:58:30 +0530

    core-mysql.sh

--- backend.sh
+++ backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 USER=$(id -u )
-TIME_STAMP=$ (date +%F-%H-%M-%S )
+TIME_STAMP=$(date +%F-%H-%M-%S )
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE=/tmp/-$SCRIPT_NAME-$TIME_STAMP.log
 echo "please enter the mysql password"
